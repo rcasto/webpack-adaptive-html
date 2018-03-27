dc3acf8cb151a6ae0b3481b7ecba5727ba5f7704
Richie Casto <ricast@microsoft.com>
2018-03-27 11:12:23 -0700

    add clean step, update adaptive-html cdn used, add use of toHTML to sample code

--- public/foo.js
+++ public/foo.js
@@ -2,3 +2,6 @@ import AdaptiveHtml from 'adaptive-html';
 
 var json = AdaptiveHtml.toJSON('<p>Turn me into an Adaptive Card</p>');
 console.log(JSON.stringify(json, null, '\t'));
+
+var cardElem = AdaptiveHtml.toHTML(json);
+console.log(cardElem.outerHTML);
