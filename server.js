c616b8e00336812c55a60de16b287305a4993be6
Richie <rcasto@users.noreply.github.com>
2018-03-26 20:29:38 -0700

    small update to server

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
